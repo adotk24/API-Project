23f6bfbacbe7a0ba62c3879b8ee23c6c891da5e0
Andrew <andrewkim1998@gmail.com>
2022-10-11 16:02:06 -0500

    small edit in root app.js

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ app.use(
     })
 );
 const routes = require('./routes');
-app.use(routes)
+app.use(routes);
 
 // Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
