67587bf446e695efcb3c1c9bb21777ec2d83cd3e
Andrew <andrewkim1998@gmail.com>
2022-10-11 15:48:11 -0500

    fixing seeder files

--- backend/db/seeders/20221011161445-demo-user.js
+++ backend/db/seeders/20221011161445-demo-user.js
@@ -5,16 +5,22 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
+        firstName: 'Andrew',
+        lastName: 'Kim',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Esther',
+        lastName: 'Kim',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: "Myong",
+        lastName: "Kim",
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
