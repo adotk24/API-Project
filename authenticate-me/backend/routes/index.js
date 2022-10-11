002cade81d91592e526ea18cc20476e5491e40d2
Andrew <andrewkim1998@gmail.com>
2022-10-11 01:54:26 -0500

    removed test route

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+//test route
+// router.get('/hello/world', function (req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
 
 // backend/routes/index.js
 // ...
