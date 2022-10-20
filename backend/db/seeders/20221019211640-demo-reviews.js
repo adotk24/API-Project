9011412cb86ca92c2bd5de5ac2c619042c5bc521
Andrew <andrewkim1998@gmail.com>
2022-10-20 15:22:55 -0500

    rest of data for reviews seeder

--- backend/db/seeders/20221019211640-demo-reviews.js
+++ backend/db/seeders/20221019211640-demo-reviews.js
@@ -9,6 +9,30 @@ module.exports = {
         userId: 1,
         review: 'testing review 1',
         stars: 5
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        review: 'testing review 2',
+        stars: 4
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        review: 'testing review 3',
+        stars: 3
+      },
+      {
+        spotId: 4,
+        userId: 4,
+        review: 'testing review 4',
+        stars: 2
+      },
+      {
+        spotId: 5,
+        userId: 5,
+        review: 'testing review 5',
+        stars: 1
       }
     ])
   },
@@ -17,7 +41,7 @@ module.exports = {
     await queryInterface.bulkDelete('Reviews', {
       spotId: {
         [Op.in]: [
-          1
+          1, 2, 3, 4, 5
         ]
       }
     })
