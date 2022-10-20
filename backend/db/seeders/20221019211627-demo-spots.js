2e4369934bbd02b891d850f240d4abeeda0c2983
Andrew <andrewkim1998@gmail.com>
2022-10-20 15:16:46 -0500

    add seeder files to spots

--- backend/db/seeders/20221019211627-demo-spots.js
+++ backend/db/seeders/20221019211627-demo-spots.js
@@ -13,10 +13,58 @@ module.exports = {
         country: "United States",
         longitude: "-77.69",
         latitude: "37.39",
-        name: 'Tomahawk Creek',
+        name: 'Ash Lake',
         description: 'this is a test description',
         price: 340
-      }
+      },
+      {
+        ownerId: 2,
+        address: '2400 Tomahawk Meadows Dr',
+        city: 'Midlothian',
+        state: "Virginia",
+        country: "United States",
+        longitude: "64.69",
+        latitude: "-37.39",
+        name: 'Tomahawk Creek',
+        description: 'this is a test description -- 2',
+        price: 500
+      },
+      {
+        ownerId: 3,
+        address: '846 Dalmalley Lane',
+        city: 'Coppell',
+        state: "Texas",
+        country: "United States",
+        longitude: "86.69",
+        latitude: "-44.39",
+        name: "Grandma's House",
+        description: 'this is a test description -- 3',
+        price: 600
+      },
+      {
+        ownerId: 4,
+        address: '1301 Crossing Pl',
+        city: 'Austin',
+        state: "Texas",
+        country: "United States",
+        longitude: "65.69",
+        latitude: "-38.30",
+        name: '',
+        description: 'this is a test description -- 4',
+        price: 100
+      },
+      {
+        ownerId: 5,
+        address: '6th Street',
+        city: 'Austin',
+        state: "Texas",
+        country: "United States",
+        longitude: "-88.69",
+        latitude: "-51.39",
+        name: 'The Bar',
+        description: 'this is a test description-- 5',
+        price: 30
+      },
 
     ])
   },
