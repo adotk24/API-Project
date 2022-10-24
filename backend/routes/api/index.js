f8125c75294be3e79d66977ea26ab598629953d7
Andrew <andrewkim1998@gmail.com>
2022-10-24 13:48:20 -0500

    fixing some bugs in index routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,7 +5,7 @@ const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 const reviewImagesRouter = require("./review-images.js");
 const reviewsRouter = require("./reviews.js");
-const spotImagesRouter = require("./spot-images");
+const spotImagesRouter = require("./spot-images.js");
 const spotsRouter = require("./spots.js");
 const bookingsRouter = require("./bookings.js");
 // backend/routes/api/index.js
@@ -13,14 +13,12 @@ const bookingsRouter = require("./bookings.js");
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
-
 router.use("/bookings", bookingsRouter);
+router.use('/spots', spotsRouter);
 router.use("/review-images", reviewImagesRouter);
 router.use("/reviews", reviewsRouter);
-router.use("/spot-images", spotImagesRouter)
-router.use('/spots', spotsRouter);
+router.use("/spot-images", spotImagesRouter);
 
 // testing router endpoint
 router.get('/test', (req, res) => {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
 
 //test route
 // router.get('/hello/world', function (req, res) {
@@ -20,9 +22,7 @@ router.get("/api/csrf/restore", (req, res) => {
 });
 // backend/routes/index.js
 // ...
-const apiRouter = require('./api');
 
-router.use('/api', apiRouter);
 
 
 module.exports = router;
