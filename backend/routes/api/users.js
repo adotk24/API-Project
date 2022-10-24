6877535b1fdbc01ba3a74bcf80f510ac88e27f1a
Andrew <andrewkim1998@gmail.com>
2022-10-24 13:58:30 -0500

    pulled first and last name from req body for sign up

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -34,7 +34,7 @@ router.post(
     '/',
     validateSignup,
     async (req, res) => {
-        const { email, password, username } = req.body;
+        const { firstName, lastname, email, password, username } = req.body;
         const user = await User.signup({ firstName, lastName, email, username, password });
 
         await setTokenCookie(res, user);
