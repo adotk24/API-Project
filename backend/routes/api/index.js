95cab5a51204c0c3370ce1801a45370d685dea61
Andrew <andrewkim1998@gmail.com>
2022-10-24 12:24:49 -0500

    added routers to api/index route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,11 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
+const reviewImagesRouter = require("./review-images.js");
+const reviewsRouter = require("./reviews.js");
+const spotImagesRouter = require("./spot-images");
+const spotsRouter = require("./spots.js");
+const bookingsRouter = require("./bookings.js");
 // backend/routes/api/index.js
 
 router.use(restoreUser);
@@ -10,6 +15,12 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use("/bookings", bookingsRouter);
+router.use("/review-images", reviewImagesRouter);
+router.use("/reviews", reviewsRouter);
+router.use("/spot-images", spotImagesRouter)
+router.use('/spots', spotsRouter);
 
 // testing router endpoint
 router.get('/test', (req, res) => {
