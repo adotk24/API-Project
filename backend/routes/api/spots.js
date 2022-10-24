e28f0aa125d5cfc980455e2899e042c55412aa40
Andrew <andrewkim1998@gmail.com>
2022-10-24 12:31:08 -0500

    added required models to spots api route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { handleValidationErrors } = require('../../utils/validation');
+const {
+    User,
+    Spot,
+    SpotImage,
+    ReviewImage,
+    Booking,
+    Review
+} = require('../../db/models');
