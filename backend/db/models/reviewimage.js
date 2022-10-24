44a3532a318adf7a9e229dc539a5402afdd52ce4
Andrew <andrewkim1998@gmail.com>
2022-10-24 12:16:36 -0500

    added defaultScope to reviewImage model

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -25,6 +25,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'ReviewImage',
+    defaultScope: { attributes: { exclude: ['reviewId', 'createdAt', 'updatedAt'] } }
   });
   return ReviewImage;
 };
