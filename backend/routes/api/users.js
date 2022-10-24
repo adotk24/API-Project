b6e3a203855f276874a49c2a3f90a39a778be8b5
Andrew <andrewkim1998@gmail.com>
2022-10-24 13:55:27 -0500

    added first and last name to user signup in users route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -35,7 +35,7 @@ router.post(
     validateSignup,
     async (req, res) => {
         const { email, password, username } = req.body;
-        const user = await User.signup({ email, username, password });
+        const user = await User.signup({ firstName, lastName, email, username, password });
 
         await setTokenCookie(res, user);
 
