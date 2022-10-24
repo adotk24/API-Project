597a20d45c423c5065163c34ffb922cd3bf18c2c
Andrew <andrewkim1998@gmail.com>
2022-10-24 12:28:08 -0500

    added required models to api/bookings route

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { handleValidationErrors } = require('../../utils/validation');
+const {
+    User,
+    Spot,
+    SpotImage,
+    ReviewImage,
+    Booking,
+} = require('../../db/models');
