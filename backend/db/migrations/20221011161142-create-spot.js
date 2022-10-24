aecb5d70e4f7e243cee2bdee66b397aaf36af1aa
Andrew <andrewkim1998@gmail.com>
2022-10-24 11:51:53 -0500

    added allowNull constraints to createSpot migrations

--- backend/db/migrations/20221011161142-create-spot.js
+++ backend/db/migrations/20221011161142-create-spot.js
@@ -15,16 +15,20 @@ module.exports = {
         onDelete: 'CASCADE'
       },
       address: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       city: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       state: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       country: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       latitude: {
         type: Sequelize.DECIMAL
@@ -33,13 +37,16 @@ module.exports = {
         type: Sequelize.DECIMAL
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       price: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
