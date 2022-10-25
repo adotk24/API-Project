de94935e7ec9df0e2b53e39c4d5a6969e64c60f0
Andrew <andrewkim1998@gmail.com>
2022-10-25 12:54:20 -0500

    get all spots provisional done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -23,21 +23,25 @@ router.get('/', async (req, res, next) => {
 
     const spots = await Spot.findAll({ ...pagination });
     let results = [];
-    spots.forEach(async (spot) => {
-        spot = spot.toJSON()
+
+    for (let i = 0; i < spots.length; i++) {
+        let spot = spots[i];
+        spot = spot.toJSON();
         const avgRating = await Review.findAll({
+            raw: true,
             where: { spotId: spot.id },
             attributes: [[Sequelize.fn('AVG', Sequelize.col('stars')), 'avgRating']]
         });
 
         const previewImage = await SpotImage.findAll({
+            raw: true,
             where: { preview: true, spotId: spot.id },
             attributes: ['url']
         });
-        if (avgRating) spot.avgRating = avgRating;
-        if (previewImage) spot.previewImage = previewImage.url;
+        if (avgRating) spot.avgRating = avgRating[0].avgRating.toFixed(1);
+        if (previewImage.length) spot.previewImage = previewImage[0].url;
         results.push(spot)
-    })
+    }
 
     return res.json({ 'Spots': results })
 });
