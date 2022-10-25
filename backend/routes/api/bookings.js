252afbd3ed3b93a8a21161f308adacaf9d53b3d5
andrew <andrewkim1998@gmail.com>
2022-10-24 19:36:13 -0500

    pretty up code

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,14 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { handleValidationErrors } = require('../../utils/validation');
-const {
-    User,
-    Spot,
-    SpotImage,
-    ReviewImage,
-    Booking,
-    Review
-} = require('../../db/models');
+const {User, Spot, SpotImage, ReviewImage, Booking, Review} = require('../../db/models');
 const { check, param } = require("express-validator");
 const {
     setTokenCookie,
--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,20 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { handleValidationErrors } = require('../../utils/validation');
-const {
-    User,
-    Spot,
-    SpotImage,
-    ReviewImage,
-    Booking,
-    Review
-} = require('../../db/models');
-const { check, param } = require("express-validator");
-const {
-    setTokenCookie,
-    requireAuth,
-    restoreUser,
-} = require("../../utils/auth");
-const { Op } = require("sequelize");
+const {User, Spot, SpotImage, ReviewImage, Booking, Review} = require('../../db/models');
+const {check, param} = require("express-validator");
+const {setTokenCookie, requireAuth, restoreUser} = require("../../utils/auth");
+const {Op} = require("sequelize");
 
 module.exports = router;
