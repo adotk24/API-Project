5d6208c5dcb5b8751aa0ea594dbc811467c6175d
Andrew <andrewkim1998@gmail.com>
2022-10-25 11:29:16 -0500

    import sequelize to my routes

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { handleValidationErrors } = require('../../utils/validation');
-const {User, Spot, SpotImage, ReviewImage, Booking, Review} = require('../../db/models');
+const { User, Spot, SpotImage, ReviewImage, Booking, Review, Sequelize } = require('../../db/models');
 const { check, param } = require("express-validator");
-const {setTokenCookie, requireAuth, restoreUser} = require("../../utils/auth");
+const { setTokenCookie, requireAuth, restoreUser } = require("../../utils/auth");
 const { Op } = require("sequelize");
 
 module.exports = router;
