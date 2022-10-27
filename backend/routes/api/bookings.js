5fb617d913bce443500aa92b31fc332c13777948
Andrew <andrewkim1998@gmail.com>
2022-10-27 02:36:29 -0500

    postman edit booking/:bookingId done

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -6,6 +6,7 @@ const { check, param } = require("express-validator");
 const { setTokenCookie, requireAuth, restoreUser } = require("../../utils/auth");
 const { Op } = require("sequelize");
 
+//get all current users bookings
 router.get('/current', requireAuth, async (req, res, next) => {
     const curBookings = await Booking.findAll({
         where: { userId: req.user.id },
@@ -21,6 +22,20 @@ router.get('/current', requireAuth, async (req, res, next) => {
         if (picUrl) itBooking.dataValues.previewImage = url
     }
     res.json({ Bookings: curBookings })
+});
+
+//edit booking
+router.put('/:bookingId', requireAuth, async (req, res, next) => {
+    let { startDate, endDate } = req.body;
+    const edittedBooking = await Booking.findByPk(req.params.bookingId);
+    if (!edittedBooking) res.json({ message: "Booking couldn't be found", statusCode: 404 });
+    edittedBooking.startDate = startDate;
+    edittedBooking.endDate = endDate
+
+
+
+    res.json(edittedBooking)
+
 })
 
 
