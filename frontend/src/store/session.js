2e00bcd177c67608cd88a395e96c743f21ab860f
Andrew <andrewkim1998@gmail.com>
2022-11-11 13:52:12 -0600

    logout done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -46,6 +46,13 @@ export const signup = (user) => async (dispatch) => {
     dispatch(setUser(data));
     return response;
 };
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
 
 const initialState = { user: null };
 
