f79e06ffa4edd2a563323dd720dfa8746e9dc05e
Andrew <andrewkim1998@gmail.com>
2022-11-11 17:34:03 -0600

    added spots action creators

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -6,3 +6,27 @@ const LOAD_USER_SPOTS = 'spot/LOAD_USER_SPOTS';
 const ADD_SPOT = 'spot/ADD_SPOT';
 const UPDATE_SPOT = 'spot/UPDATE_SPOT';
 const DELETE_SPOT = '/spot/DELETE_SPOT';
+
+const loadSpots = spots => {
+    type: LOAD_SPOTS, spots
+};
+
+const loadOneSpot = spot => {
+    type: LOAD_ONE_SPOT, spot
+};
+
+const loadUserSpots = spots => {
+    type: LOAD_USER_SPOTS, spots
+};
+
+const addSpot = spot => {
+    type: ADD_SPOT, spot
+};
+
+const updateSpot = spot => {
+    type: UPDATE_SPOT, spot
+};
+
+const deleteSpot = spot => {
+    type: DELETE_SPOT, spot
+}
