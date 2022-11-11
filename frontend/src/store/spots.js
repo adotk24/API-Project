9d768149e87d3cb9e59a809a533c3b1a2a42c491
Andrew <andrewkim1998@gmail.com>
2022-11-11 17:30:12 -0600

    added action type in spots

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,1 +1,8 @@
 import { csrfFetch } from "./csrf";
+
+const LOAD_SPOTS = 'spots/LOAD_SPOTS';
+const LOAD_ONE_SPOT = 'spot/LOAD_ONE_SPOT';
+const LOAD_USER_SPOTS = 'spot/LOAD_USER_SPOTS';
+const ADD_SPOT = 'spot/ADD_SPOT';
+const UPDATE_SPOT = 'spot/UPDATE_SPOT';
+const DELETE_SPOT = '/spot/DELETE_SPOT';
