066d98d1ceec2136793a166a385d4e3f1840ed27
Andrew <andrewkim1998@gmail.com>
2022-11-11 18:10:31 -0600

    spot thunks prelim done

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -29,4 +29,53 @@ const updateSpot = spot => {
 
 const deleteSpot = spot => {
     type: DELETE_SPOT, spot
+};
+
+export const getAllSpots = () => async dispatch => {
+    const response = await csrfFetch('/api/spots');
+    if (response.ok) {
+        const allSpots = await response.json();
+        dispatch(loadSpots(getAllSpots));
+        return allSpots
+    }
+};
+
+export const getOneSpot = (id) => async dispatch => {
+    const response = await csrfFetch(`/api/spots/${id}`);
+    if (response.ok) {
+        const spot = await response.json();
+        dispatch(loadOneSpot(spot));
+        return spot
+    }
+};
+
+export const addingSpot = spot => async dispatch => {
+    const response = await csrfFetch('/api/spots', {
+        method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(spot)
+    });
+    if (response.ok) {
+        const addedSpot = await response.json();
+        dispatch(addSpot(addedSpot));
+        return addedSpot
+    }
+};
+
+export const editSpot = (id, spot) => async dispatch => {
+    const response = await csrfFetch(`/api/spots/${id}`, {
+        method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(spot)
+    });
+    if (response.ok) {
+        const editedSpot = await response.json();
+        dispatch(updateSpot(editedSpot));
+        return editSpot
+    }
+};
+
+export const deletingSpot = id => async dispatch => {
+    const response = await csrfFetch(`/api/spots/${id}`, { method: 'DELETE' });
+    if (response.ok) {
+        const deletedSpot = await response.json();
+        dispatch(deleteSpot(id));
+        return deleteSpot
+    }
 }
