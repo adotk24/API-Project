07caa8d039787e9735cf96aee6af03a2730ab5bd
Andrew <andrewkim1998@gmail.com>
2022-11-14 15:29:56 -0600

    fixed refresh => logout problem

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -68,7 +68,7 @@ router.delete(
                 const lastName = user.lastName;
                 const email = user.email;
                 const username = user.username;
-                return res.json(user);
+                return res.json({ user });
             } else return res.json({ user: null });
         }
     );
