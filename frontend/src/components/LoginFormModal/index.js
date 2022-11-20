55a940ecbb2671abd5e3fb0cbf38ab637251d1ab
Andrew <andrewkim1998@gmail.com>
2022-11-20 01:01:15 -0600

    starting navigation css bugt

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import LoginForm from './LoginForm';
+import './LoginForm.css'
+
 
 function LoginFormModal() {
     const [showModal, setShowModal] = useState(false);
 
     return (
         <>
-            <button onClick={() => setShowModal(true)}>Log in</button>
+            <button
+                className='button-to-log-in'
+                onClick={() => setShowModal(true)}>Log in</button>
             {showModal && (
                 <Modal onClose={() => setShowModal(false)}>
                     <LoginForm setShowModal={setShowModal} />
