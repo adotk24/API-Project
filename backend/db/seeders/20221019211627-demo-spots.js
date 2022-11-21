92accbbc1e483381ec227174372757980365ccb2
Andrew <andrewkim1998@gmail.com>
2022-11-20 22:57:35 -0600

    added more seeder data

--- backend/db/seeders/20221019211627-demo-spots.js
+++ backend/db/seeders/20221019211627-demo-spots.js
@@ -30,7 +30,7 @@ module.exports = {
         price: 46
       },
       {
-        ownerId: 2,
+        ownerId: 1,
         address: '471 Farming St',
         city: 'Cedar City',
         state: "Utah",
@@ -42,7 +42,7 @@ module.exports = {
         price: 145
       },
       {
-        ownerId: 3,
+        ownerId: 1,
         address: 'Really High in the Air(bnb)',
         city: 'Broad Cove',
         state: "Nova Scotia",
@@ -54,7 +54,7 @@ module.exports = {
         price: 100
       },
       {
-        ownerId: 4,
+        ownerId: 1,
         address: '1601 Wow Way',
         city: 'Grass Valley',
         state: "California",
@@ -64,7 +64,176 @@ module.exports = {
         name: 'Scenic A-Frame retreat',
         description: "Tucked in the foothills of the quaint historic mining town of Grass Valley, CA this funky and spacious chalet invites you to experience the great outdoors with family, friends and pets.",
         price: 30
-      }
+      },
+      {
+        ownerId: 1,
+        address: '123 Somewhere Norway',
+        city: 'Rollag kommune',
+        state: "Vivken",
+        country: "Norway",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Stor laftet hytte med høy standard og flott utsikt',
+        description: "Hytta er av nyere dato og har en gjennomgående høy standard med fantastisk beliggenhet på 900 meters høyde.",
+        price: 588
+      },
+      {
+        ownerId: 2,
+        address: '1157 Yogurt St',
+        city: 'Rodia',
+        state: "Athens",
+        country: "Greece",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Villa Omikron',
+        description: "TMany significant words in the greek language start with the letter Omikron (O), such as “omorfia” (beauty), “ouranos” (sky), “orizontas” (horizon), “oxygono” (oxygen), “orasi” (sight) and “osfrisi” (smell) and Villa Omikron encompasses all these qualities, making you a promise to excite all your senses and make you live what you've dreamed of for your vacation in Crete!",
+        price: 672
+      },
+      {
+        ownerId: 2,
+        address: '2121 Bartolomeo Boulevard',
+        city: 'Malibu',
+        state: "California",
+        country: "United States",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Iconic Airstream x Pottery Barn Edition',
+        description: "TThe Airstream is nestled on a private property and is conveniently located near the Santa Monica Backbone Trail.",
+        price: 350
+      },
+      {
+        ownerId: 2,
+        address: '1611 Boat Dock',
+        city: 'Fort Pierce',
+        state: "Florida",
+        country: "United States",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: '9000 SF Waterfront Mansion',
+        description: "Feel like a VIP with our dedicated concierge who is always on call to help you with any extra services or reservations you may need such as a yacht, night club, restaurants, private chef, massage therapist etc.",
+        price: 280
+      },
+      {
+        ownerId: 2,
+        address: '8342 Louis Lane',
+        city: 'Saint-Ferréol',
+        state: "Quebec",
+        country: "Canada",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Initial',
+        description: "Les soeurs, c'est deux imposants chalet en bois , lumineux et convivial, positionné l'un à côté de l'autre. La soeur No1, c'est un chalet de 3 chambres, offrant de l'espace pour 8 personnes sur 2 étages, un plafond cathédrale et un foyer au salon.",
+        price: 155
+      },
+      {
+        ownerId: 2,
+        address: '632 Joanee Street',
+        city: 'Joshua Tree',
+        state: "California",
+        country: "United States",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: '3-Story Container Home',
+        description: "TWelcome to SkyBox. A fusion of multiple shipping containers has created this one-of-a-kind property in the vast Mojave desert.",
+        price: 261
+      },
+      {
+        ownerId: 2,
+        address: '825 Michelle Courtyard',
+        city: 'Smiths Beach',
+        state: "Victoria",
+        country: "Australia",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Country Meets Ocean',
+        description: "Welcome to the 'Family Escape' at Marlin Beachfront...a holiday destination that once experienced, will never be forgotten.",
+        price: 385
+      },
+      {
+        ownerId: 2,
+        address: '845 Barn Boulevard',
+        city: 'Austerlitz',
+        state: "New York",
+        country: "United States",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Renovated 4 BR 3 BA Barn',
+        description: "Discover the best of the Berkshires and Hudson Valley at the Barn at Bald Mountain! ",
+        price: 375
+      },
+      {
+        ownerId: 2,
+        address: '844 Red Cottage',
+        city: 'Callicoon Center',
+        state: "New York",
+        country: "United States",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Lilac Hill',
+        description: "Lilac Hill is a stunning escape in the heart of the Catskills, located just 2 hours from NYC.",
+        price: 289
+      },
+      {
+        ownerId: 2,
+        address: '211 Ravi Road',
+        city: 'Lafayette',
+        state: "Louisiana",
+        country: "United States",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Carriage House',
+        description: "Nestled in the shady oaks of Sterling Grove Historic District in Lafayette, our quaint Carriage House suite is tucked behind the main home. ",
+        price: 139
+      },
+      {
+        ownerId: 2,
+        address: '5332 Ryans Road',
+        city: 'Borrego Springs',
+        state: "California",
+        country: "United States",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Casa Sombrero',
+        description: "Welcome to Casa Sombrero, a little slice of desert paradise. ",
+        price: 237
+      },
+      {
+        ownerId: 2,
+        address: '734 Makayla Meadows',
+        city: 'Harbor Springs',
+        state: "Michigan",
+        country: "United States",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Waterfront Home',
+        description: "This modern waterfront residence is 10 minutes from downtown Harbor Springs featuring an open floor plan, high-end finishes, a covered deck facing Lake Michigan, a large private sandy beach, a sauna and fireplace.",
+        price: 824
+      },
+      {
+        ownerId: 2,
+        address: '8444 Natalie Road',
+        city: 'Caicos Island',
+        state: "Turks & Caicos",
+        country: "Islands",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'Villa Maris',
+        description: "Villa Maris is brand-new villa, recently constructed on the popular and highly desirable Leeward canal quarter on the Turks and Caicos Island of Providenciales. ",
+        price: 2016
+      },
+      {
+        ownerId: 2,
+        address: '814 Amber Avenue',
+        city: 'Fayetteville',
+        state: "Arkansas",
+        country: "United States",
+        lng: "-88.69",
+        lat: "-51.39",
+        name: 'The Fritz',
+        description: "TEnjoy proximity to Downtown Fayetteville, Dickson Street & Razorback Stadium. Fully renovated in 2020 in the heart of the historic district walking distance to Wilson Park. ",
+        price: 439
+      },
+
 
     ])
   },
@@ -74,7 +243,7 @@ module.exports = {
     await queryInterface.bulkDelete('Spots', {
       ownerId: {
         [Op.in]: [
-          1, 2, 3, 4, 5
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
         ]
       }
     })
--- backend/db/seeders/20221019211701-demo-spot-images.js
+++ backend/db/seeders/20221019211701-demo-spot-images.js
@@ -28,7 +28,78 @@ module.exports = {
         spotId: 5,
         url: 'https://a0.muscache.com/im/pictures/miso/Hosting-729597790487190657/original/07c2691a-7a40-4740-bf9b-6e821b52547b.jpeg',
         preview: true
+      },
+      {
+        spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-721386515699207701/original/c81d0a7d-54bf-412f-bc1e-969a69fa09b8.jpeg',
+        preview: true
+      },
+      {
+        spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-716489971524221921/original/9c440b57-1ce9-4b57-977e-25df5600bdb3.jpeg',
+        preview: true
+      },
+      {
+        spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-715990626223541790/original/694ee517-4dd7-4ace-92d6-c96ec635bdf0.jpeg',
+        preview: true
+      },
+      {
+        spotId: 9,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-734596846818164578/original/4cebb10d-c7f2-4add-9c74-58d74f683431.jpeg',
+        preview: true
+      },
+      {
+        spotId: 10,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-715401606875304002/original/f720bb71-9bd0-42d2-abc1-9ff2817fb18a.jpeg',
+        preview: true
+      },
+      {
+        spotId: 11,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-728091517814307485/original/c50089d5-c449-439a-8f19-46e959bf883a.jpeg',
+        preview: true
+      },
+      {
+        spotId: 12,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-714028222625357260/original/3337352b-891f-4fae-a1fb-76965fc2bc49.jpeg',
+        preview: true
+      },
+      {
+        spotId: 13,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-750147911122891249/original/a754a20d-406b-432c-87bb-b3d61f22c0e0.jpeg',
+        preview: true
+      },
+      {
+        spotId: 14,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-739863408028433088/original/16f11779-8d06-4eef-a893-1676e53fb8bd.jpeg',
+        preview: true
+      },
+      {
+        spotId: 15,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-710719347181825830/original/266c5b80-9b2d-48ec-8962-ec9288c27ec1.jpeg',
+        preview: true
+      },
+      {
+        spotId: 16,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-719511952691427887/original/7f4279d2-5484-4148-9265-83099651c3c4.jpeg',
+        preview: true
+      },
+      {
+        spotId: 17,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-719551181197137816/original/8caadc39-bc21-4071-acaa-02a1391fd99e.jpeg',
+        preview: true
+      },
+      {
+        spotId: 18,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-716678031078809652/original/afe98822-1733-4b93-85b6-5ebd2818df08.jpeg',
+        preview: true
+      },
+      {
+        spotId: 19,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-733963185137452410/original/200fee62-3517-4835-ba7f-169bc359e690.jpeg',
+        preview: true
       }
+
     ])
   },
 
@@ -38,7 +109,7 @@ module.exports = {
 
       spotId: {
         [Op.in]: [
-          1, 2, 3, 4, 5
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
         ]
       }
     })
